48febebfa051d3fb0214e6259c0021294a44134c
F-l-a-m <andrew.flam@gmail.com>
2018-07-18 13:22:53 +0300

    Chapter 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+My Head First PHP book code
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo '<h1><a href="report.html">Report page<a></h1>';
+        ?>
+    </body>
+</html>
